7dbc4c711101458b45792f82cf1d300a91c0b9e7
Selam25-30 <selamawitbirhanu123@gmail.com>
2024-09-12 12:24:24 -0700

    Banner and Rows

--- Reactnetflix/src/pages/Home/Home.jsx
+++ Reactnetflix/src/pages/Home/Home.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Banner from '../../components/Banner/banner'
+// import Row from '../../components/Rows/Row/Row'
+import RowList from '../../components/Rows/RowList/RowList'
+
+
+
+
 
 const Home = () => {
   return (
     <>
 
      <Header />
+
+      <Banner />
+      <RowList />
+      
      <Footer />
     
     
     </>
   )
 }
+     
 
 
 export default Home
